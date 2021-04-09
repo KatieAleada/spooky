77ba946b886f8e4284fefc52b834c499238c26ab
Katie <katie.aleada@gmail.com>
2021-04-09 10:49:03 -0700

    Add intro

--- app.R
+++ app.R
@@ -1,1 +1,15 @@
 #Rshiny tutorial 
+
+#Load Libraries
+library(tidyverse)
+library(shiny)
+library(shinythemes)
+
+# Create the user interface:
+ui <- fluidPage()
+
+# Create the server function:
+server <- function(input, output) {}
+
+# Combine them into an app:
+shinyApp(ui = ui, server = server)
